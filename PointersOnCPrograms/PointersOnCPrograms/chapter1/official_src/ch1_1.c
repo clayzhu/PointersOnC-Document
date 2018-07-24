5e3ab0674fdfc0ea64cb0f5295263d80e86d4718
zhuguang <zhuguang@corp.netease.com>
2018-07-24 09:56:11 +0800

    :tada: 添加工程和一些代码。

---
+++ PointersOnCPrograms/PointersOnCPrograms/chapter1/official_src/ch1_1.c
@@ -0,0 +1,16 @@
+//
+//  ch1_1.c
+//  PointersOnCPrograms
+//
+//  Created by Netease on 2018/7/23.
+//  Copyright © 2018年 Netease. All rights reserved.
+//
+
+#include <stdio.h>
+
+#if 0
+int main (void) {
+    printf("Hello world!\n");
+    return 0;
+}
+#endif
---
+++ PointersOnCPrograms/PointersOnCPrograms/chapter1/official_src/ch1_2.c
@@ -0,0 +1,34 @@
+//
+//  ch1_2.c
+//  PointersOnCPrograms
+//
+//  Created by Netease on 2018/7/23.
+//  Copyright © 2018年 Netease. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#if 1
+int main (void) {
+    int ch; // 输入的字符
+    int line = 1;   // 行号
+    int at_beginning = 1;   // 表示输入的是第1个字符
+    while ((ch = getchar()) != EOF) {
+        // 在输入第1个字符，先打印行号
+        if (at_beginning == 1) {
+            printf("%d", line);
+            line += 1;
+            at_beginning = 0;
+        }
+        
+        putchar(ch);
+        
+        if (ch == '\n') {
+            at_beginning = 1;
+        }
+    }
+    
+    return EXIT_SUCCESS;
+}
+#endif
