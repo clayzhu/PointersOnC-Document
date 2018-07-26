d0856fc448969cf82b99894cfb4b2835778c29e6
zhuguang <zhuguang@corp.netease.com>
2018-07-26 20:34:22 +0800

    添加 ch1_3.c 代码。

--- PointersOnCPrograms/PointersOnCPrograms/chapter1/official_src/ch1_3.c
+++ PointersOnCPrograms/PointersOnCPrograms/chapter1/official_src/ch1_3.c
@@ -11,6 +11,13 @@
 
 #if 1
 int main (void) {
+    int ch;
+    signed char checksum = -1;
+    while ((ch = getchar()) != EOF) {
+        putchar(ch);
+        checksum += ch;
+    }
+    printf("%d\n", checksum);
     return EXIT_SUCCESS;
 }
 #endif
