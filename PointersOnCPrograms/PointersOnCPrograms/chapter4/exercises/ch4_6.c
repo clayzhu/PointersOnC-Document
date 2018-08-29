69914b6709f58b06a6b1d4fedbbccadefa79c1b2
zhuguang <zhuguang@corp.netease.com>
2018-08-29 23:16:17 +0800

    习题 ch4_6.

--- PointersOnCPrograms/PointersOnCPrograms/chapter4/exercises/ch4_6.c
+++ PointersOnCPrograms/PointersOnCPrograms/chapter4/exercises/ch4_6.c
@@ -13,6 +13,30 @@
 int substr(char dst[], char src[], int start, int len);
 
 int main(void) {
+    char dst[3];
+    char src[] = "abcdefg";
+    int substr_len = substr(dst, src, 1, 3);
+    printf("substr:%s, len:%d\n", dst, substr_len);
     return EXIT_SUCCESS;
+}
+
+int substr(char dst[], char src[], int start, int len) {
+        if (start < 0 || len < 0 || src[start] == '\0') {
+            dst = "";
+            return 0;
+        }
+    
+        int dst_index = 0, src_index = start;
+        while (dst_index < len) {
+            if (src[src_index] != '\0') {   // 子串还未到 len 长度，但已取完 src 的数据
+                dst[dst_index] = src[src_index];
+                dst_index ++;
+                src_index ++;
+            } else {
+                dst[dst_index] = '\0';  // 补充子串最后一个 NUL 字符
+                break;
+            }
+        }
+        return dst_index;
 }
 #endif
