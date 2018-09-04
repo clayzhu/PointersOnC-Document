fc41bdc596c551c0fce52607e9991527260d5227
zhuguang <zhuguang@corp.netease.com>
2018-09-04 13:58:19 +0800

    示例代码 ch5.

--- PointersOnCPrograms/PointersOnCPrograms/chapter5/official_src/count_1b.c
+++ PointersOnCPrograms/PointersOnCPrograms/chapter5/official_src/count_1b.c
@@ -11,12 +11,12 @@ count_one_bits( unsigned value )
 	/*
 	** While the value still has some 1-bits in it ...
 	*/
-	for( ones = 0; value != 0; value = value >> 1 )
+	for( ones = 0; value != 0; value >>= 1 )
 		/*
 		** If the low-order bit is a 1, count it.
 		*/
-		if( value % 2 != 0 )
-			ones = ones + 1;
+		if( ( value & 1 ) != 0 )
+			ones += 1;
 
 	return ones;
 }
