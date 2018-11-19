34d1b1e1ed877f4b35f42ef0807b461630b05a44
zhuguang <zhuguang@corp.netease.com>
2018-11-19 21:12:46 +0800

    习题 ch5_1 另一种解法。

---
+++ PointersOnCPrograms/PointersOnCPrograms/chapter5/exercises/ch5_1b.c
@@ -0,0 +1,24 @@
+//
+//  ch5_1b.c
+//  PointersOnCPrograms
+//
+//  Created by Netease on 2018/11/19.
+//  Copyright © 2018 Netease. All rights reserved.
+//
+
+#if 1
+// 习题 ch5_1 另一种解法
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int ch;
+    while ((ch = getchar()) != EOF) {
+        if ('A' <= ch && ch <= 'Z') {
+            ch += 'a' - 'A';
+        }
+        putchar(ch);
+    }
+    return EXIT_SUCCESS;
+}
+#endif
