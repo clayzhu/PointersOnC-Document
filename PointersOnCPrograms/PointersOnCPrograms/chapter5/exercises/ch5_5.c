c4ccec933bab2f2835012f82949a528e96930af3
zhuguang <zhuguang@corp.netease.com>
2018-11-22 00:07:33 +0800

    ...

--- PointersOnCPrograms/PointersOnCPrograms/chapter5/exercises/ch5_5.c
+++ PointersOnCPrograms/PointersOnCPrograms/chapter5/exercises/ch5_5.c
@@ -6,4 +6,15 @@
 //  Copyright © 2018 Netease. All rights reserved.
 //
 
+#if 1
 #include <stdio.h>
+#include <stdlib.h>
+
+int store_bit_field(int original_value, int value_to_store, unsigned starting_bit, unsigned ending_bit) {
+    return 0;
+}
+
+int main(void) {
+    return EXIT_SUCCESS;
+}
+#endif
