6969e856c6103c9c56dceb445d0a77c8b31cde73
zhuguang <zhuguang@corp.netease.com>
2018-12-04 22:03:43 +0800

    习题 ch5_5.

--- PointersOnCPrograms/PointersOnCPrograms/chapter5/exercises/ch5_5.c
+++ PointersOnCPrograms/PointersOnCPrograms/chapter5/exercises/ch5_5.c
@@ -11,10 +11,33 @@
 #include <stdlib.h>
 
 int store_bit_field(int original_value, int value_to_store, unsigned starting_bit, unsigned ending_bit) {
-    return 0;
+    int length_to_store = starting_bit - ending_bit + 1; // 要存储的位数
+    unsigned mask = 0;
+    // 步骤 1
+    // 先移出掩码中的1，后面多个0
+    for (int i = 0; i < length_to_store; i ++) {
+        mask |= 1;
+        mask <<= 1;
+    }
+    // 移出掩码后面剩余的0（上面已经移出1个0，这里少移1个）
+    for (int i = 0; i < ending_bit - 1; i ++) {
+        mask |= 0;
+        mask <<= 1;
+    }
+    // 步骤2
+    original_value &= ~mask;
+    // 步骤3
+    value_to_store <<= ending_bit;
+    // 步骤4
+    value_to_store &= mask;
+    // 步骤5
+    unsigned result = value_to_store | original_value;
+    return result;
 }
 
 int main(void) {
+    int store_bit = store_bit_field(0xffff, 0x123, 13, 9);
+    printf("%x\n", store_bit);
     return EXIT_SUCCESS;
 }
 #endif
